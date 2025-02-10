a825bec86d1dc03b9e7d01495d055bd02fd37af7
DyaZix-Code <blancosacha38@gmail.com>
2025-02-10 08:31:42 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-btn>COUCOU</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+</script>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+// src/main.js
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createPinia } from 'pinia'
+import vuetify from './plugins/vuetify'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(vuetify)
+
+app.mount('#app')
