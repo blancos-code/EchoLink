984b1835cd55cafbac683b5a1637fbc35252fcec
DyaZix-Code <blancosacha38@gmail.com>
2025-02-10 14:13:41 +0100

    fix: routing

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,66 +1,81 @@
 <template>
   <v-app>
-      <v-layout>
-        <v-navigation-drawer
-            permanent
-        >
-          <template v-slot:prepend>
-            <div
+    <v-layout>
+      <v-navigation-drawer
+          permanent
+      >
+        <template v-slot:prepend>
+          <div
               class="d-inline-flex"
-            >
-              <v-img
-                  src="/src/assets/images/logo.png"
-                  alt=""
-                  width="28px"
-                  class="ml-2"
-              />
-              <v-card-title class="mt-1">
-                EchoLink
-              </v-card-title>
-            </div>
-          </template>
+          >
+            <v-img
+                src="/src/assets/images/logo.png"
+                alt=""
+                width="28px"
+                class="ml-2"
+            />
+            <v-card-title class="mt-1">
+              EchoLink
+            </v-card-title>
+          </div>
+        </template>
 
-          <template v-slot:append>
-            <v-list-item
-                lines="two"
-                prepend-avatar="https://randomuser.me/api/portraits/women/81.jpg"
-                subtitle="En ligne"
-                title="Jane Smith"
-            >
-              <template #append>
-                <v-btn
+        <template v-slot:append>
+          <v-list-item
+              lines="two"
+              prepend-avatar="https://randomuser.me/api/portraits/women/81.jpg"
+              subtitle="En ligne"
+              title="Jane Smith"
+          >
+            <template #append>
+              <v-btn
                   icon="mdi-bell"
                   variant="text"
-                />
-              </template>
-            </v-list-item>
-            <div class="pa-2">
-              <v-btn
+              />
+            </template>
+          </v-list-item>
+          <div class="pa-2">
+            <v-btn
                 block
                 variant="flat"
                 color="error"
-              >
-                Déconnexion
-              </v-btn>
-            </div>
-          </template>
+            >
+              Déconnexion
+            </v-btn>
+          </div>
+        </template>
 
-          <v-divider></v-divider>
+        <v-divider></v-divider>
 
-          <v-list density="compact" nav>
-            <v-list-item prepend-icon="mdi-map" title="Carte des urgences" value="map"></v-list-item>
-            <v-list-item prepend-icon="mdi-message" title="Messages" value="messages"></v-list-item>
-            <v-list-item prepend-icon="mdi-forum" title="Forum" value="forum"></v-list-item>
-            <v-list-item prepend-icon="mdi-podium" title="Classements" value="ranking"></v-list-item>
-          </v-list>
-        </v-navigation-drawer>
-        <v-main>
-          <v-container fluid>
-            <router-view />
-          </v-container>
-        </v-main>
-      </v-layout>
+        <v-list density="compact" nav>
+          <router-link
+              v-for="route in routes"
+              :to="route.name"
+              style="color: black; text-decoration: none;"
+          >
+            <v-list-item
+                :prepend-icon="route.icon"
+                :title="route.title"
+                :value="route.name"
+            />
+          </router-link>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main>
+        <v-container fluid>
+          <router-view/>
+        </v-container>
+      </v-main>
+    </v-layout>
   </v-app>
 </template>
-<script setup lang="ts">
+<script setup>
+import {ref} from "vue";
+
+const routes = ref([
+  {title: 'Carte des urgences', icon: 'mdi-map', name: 'map'},
+  {title: 'Messages', icon: 'mdi-message', name: 'messages'},
+  {title: 'Forum', icon: 'mdi-forum', name: 'forum'},
+  {title: 'Classement', icon: 'mdi-podium', name: 'classement'},
+])
 </script>
--- src/router/router.js
+++ src/router/router.js
@@ -1,18 +1,40 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import {createMemoryHistory, createRouter, createWebHistory} from 'vue-router'
 import Layout from '@/layout/Layout.vue'
 import Map from '@/views/Map.vue'
+import Messages from '@/views/Messages.vue'
+import Forum from '@/views/Forum.vue'
+import Classement from "@/views/Classement.vue";
 
 const routes = [
     {
         path: '/',
         component: Layout,
         children: [
-            { path: '', component: Map }
+            {
+                path: '/map',
+                name: 'map',
+                component: Map
+            },
+            {
+                path: '/messages',
+                name: 'messages',
+                component: Messages
+            },
+            {
+                path: '/forum',
+                name: 'forum',
+                component: Forum
+            },
+            {
+                path: '/classement',
+                name: 'classement',
+                component: Classement
+            }
         ]
     },
 ]
 
 export const router = createRouter({
-    history: createMemoryHistory(),
+    history: createWebHistory(),
     routes,
 })
