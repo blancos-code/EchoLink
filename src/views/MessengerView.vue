47444f0463dc3db2ebb267ad0f4f350b4f97f824
JeejaFR <jeremy.defuentes@gmail.com>
2025-02-10 15:01:36 +0100

    feat: creation conv

--- src/views/MessengerView.vue
+++ src/views/MessengerView.vue
@@ -1,12 +1,12 @@
 <template>
   <v-layout fill-height column>
     <conversation-list v-model="drawer" :chats="chats" :selected-chat-id="selectedChat?.id"
-      @chat-selected="handleChatSelect" />
+      @chat-selected="handleChatSelect" @create-chat="showCreateChatForm = true"/>
 
     <v-main fill-height>
       <template v-if="selectedChat">
         <chat-window :selectedChat="selectedChat"
-          @send-message="handleSendMessage" />
+          @send-message="handleSendMessage" @back="handleBack"/>
       </template>
       <v-container v-else class="d-flex align-center justify-center" fluid>
         <div class="text-center">
@@ -14,6 +14,21 @@
           <div class="text-h6 mt-4 text-grey">Select a conversation to start messaging</div>
         </div>
       </v-container>
+
+      <!-- Create Chat Form -->
+      <v-dialog v-model="showCreateChatForm" max-width="400px">
+        <v-card>
+          <v-card-title>Create a new chat</v-card-title>
+          <v-card-text>
+            <v-text-field v-model="newChatName" label="Chat Name" required></v-text-field>
+            <v-text-field v-model="newChatAvatar" label="Avatar URL" required></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn text @click="showCreateChatForm = false">Cancel</v-btn>
+            <v-btn color="primary" @click="createChat">Create</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-main>
   </v-layout>
 </template>
@@ -90,13 +105,20 @@ export default {
             },
           ]
         }
-      ]
+      ],
+      // Data for creating a new chat
+      showCreateChatForm: false,
+      newChatName: '',
+      newChatAvatar: ''
     }
   },
 
   methods: {
     handleChatSelect(chat) {
       this.selectedChat = chat
+    },
+    handleBack(){
+      this.selectedChat = null
     },
     handleSendMessage(message) {
       if (!this.selectedChat) return
@@ -114,8 +136,32 @@ export default {
         hour: '2-digit',
         minute: '2-digit'
       })
+    },
+    // Function to create a new chat
+    createChat() {
+      if (!this.newChatName || !this.newChatAvatar) {
+        // Make sure we have both name and avatar
+        return alert('Please provide both a name and an avatar URL')
+      }
+
+      // Generate a unique ID for the new chat (could be dynamic if needed)
+      const newChatId = this.chats.length + 1;
+
+      // Add the new chat to the chats array
+      this.chats.push({
+        id: newChatId,
+        name: this.newChatName,
+        avatar: this.newChatAvatar,
+        lastMessage: '',
+        time: '',
+        messages: []
+      });
+
+      // Reset the form
+      this.newChatName = '';
+      this.newChatAvatar = '';
+      this.showCreateChatForm = false;
     }
   }
 }
 </script>
-
